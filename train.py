e400ecb9f3e10bf4a205d80b083061ad82af6b4f
benihime91 <aburagohain160@gmail.com>
2020-09-19 03:14:26 +0530

    minor fix

--- train.py
+++ train.py
@@ -103,8 +103,8 @@ def _get_logger(name=None):
     formatter = _ColorfulFormatter(
         colored("[%(asctime)s %(name)s]: ", "green") + "%(message)s",
         datefmt="%m/%d %H:%M:%S",
-        root_name="retinanet",
-        abbrev_name=str("retinanet"),
+        root_name="retinanet_pet_detector",
+        abbrev_name=str("rpd"),
     )
 
     ch.setFormatter(formatter)
