11d1805ce4bb9117766fb8445d59ac3b72cb9704
benihime91 <aburagohain160@gmail.com>
2020-09-20 20:05:40 +0530

    update app

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from references.utils import get_model, get_preds, label_dict, load_yaml_config
 
 warnings.filterwarnings("ignore")
 
+# do computation on CPU
 DEVICE = "cpu"
 # Instantiate the visualizer
 viz = Visualizer(label_dict)
@@ -42,74 +43,76 @@ def load_image() -> np.array:
 # Draw prediciton on the given image
 def draw_preds_on_image(uploaded_image, boxes, labels, scores):
     "draws predicitons on the Given Image"
-    image = viz.draw_bboxes(
-        uploaded_image, boxes, labels, scores, save=False, show=False, return_fig=True
-    )
+    image = viz.draw_bboxes(uploaded_image, boxes, labels, scores, save=False, show=False, return_fig=True)
     return image
 
 
-def start_app() -> None:
-    st.title("Detecting Pet Faces 👁")
-    st.write(
-        "This application detects the faces of some common Pet Breeds using a [RetinaNet](https://github.com/benihime91/pytorch_retinanet)."
-    )
+def _init_app() -> None:
+    """
+    Fn that Initializes the App.
+    Prints some Fancy Stuff to StreamLit page, Display demo Image and train data stats
+    """
+    # set title
+    st.title("Detecting Pet Faces 👁 🐶 🐱",)
+    st.markdown("This application detects the faces of some common Pet Breeds using a **RetinaNet**.")
     st.write("## How does it work?")
-    st.write(
-        "Add an image of a pet (cat or dog) and the app will draw the dounding box where it detects the objects:"
-    )
-    st.image(
-        Image.open("images/res_1.png"),
-        caption="Example of model being on the Image of a dog [breed: leonburger].",
-        use_column_width=True,
-    )
+    st.write("Add an image of a pet (cat or dog) and the app will draw the dounding box where it detects the objects:")
+    # Display demo Image
+    st.image(Image.open("images/res_3.png"), caption="Example", use_column_width=True,)
     st.write("## Upload your own image")
     st.write(
-        "**Note:** The model has been trained on pets breeds given in the [The Oxford-IIIT Pet Dataset](https://www.robots.ox.ac.uk/~vgg/data/pets/)"
-        " and therefore will only with those kind of images."
+    "**Note:** The model has been trained on pets breeds given in the `The Oxford-IIIT Pet Dataset`"
+    " and therefore will only with those kind of images."
     )
     st.markdown("**To be more precise the model has been trained on these breeds:**")
-    st.image(
-        Image.open("images/breed_count.jpg"),
-        caption="Train Data Statistics ",
-        use_column_width=True,
-    )
-    st.markdown("[pic credits](https://www.robots.ox.ac.uk/~vgg/data/pets/)")
+    # Show Train data Statistics
+    st.image(Image.open("images/breed_count.jpg"),use_column_width=True,)
+
 
 def main() -> None:
     # Start the app
-    start_app()
+    _init_app()
 
     # Load in the Image
     image = load_image()
 
+    st.sidebar.markdown("# Set Detection Parameters")
+    # select model architecture
+    st.sidebar.markdown("**Select Model Architeture:**")
+    model_arch = st.sidebar.selectbox(
+        " Currently supports RetinaNet model with resnet50 & resnet34 backbone :",
+        ("resnet50", "resnet34"),
+    )
+    ps = {"resnet50": "config/resnet50.yaml", "resnet34": "config/resnet34.yaml"}
+    _path = ps[model_arch]
+
+    # Set Score threshold
+    st.sidebar.markdown("**Score threshold to filter detections**")
+    score_threshold = st.sidebar.slider(
+        label="Detections with score < score_threshold are discarded",
+        min_value=0.1,
+        max_value=1.0,
+        value=0.7,
+    )
+
+    # Set IOU Threshold
+    st.sidebar.markdown("**iou threshold for detection bounding boxes**")
+    nms_thres = st.sidebar.slider(
+        label="iou threshold", min_value=0.1, max_value=1.0, value=0.4,
+    )
+
+    # Set maximum detections
+    st.sidebar.markdown(
+        "**Max number of bounding boxes to be detected in the given Image**"
+    )
+    md = st.sidebar.slider(
+        label="max detections", min_value=1, max_value=500, value=100,
+    )
+
     # If image is Upladed make predictions when button is clicked
     if image is not None:
-        st.markdown("## Set Detection Parameters")
-
-        score_threshold = st.slider(
-            label="score threshold for detections (Detections with score < score_threshold are discarded)",
-            min_value=0.1,
-            max_value=1.0,
-            value=0.7,
-        )
-
-        nms_thres = st.slider(
-            label="iou threshold for detection bounding boxes",
-            min_value=0.1,
-            max_value=1.0,
-            value=0.4,
-        )
-
-        md = st.slider(
-            label="max number of bounding boxes to be detected in the Image",
-            min_value=1,
-            max_value=500,
-            value=100,
-        )
-
-        _path = "config/resnet34.yaml"
-
         if st.button("Generate predictions"):
+            # Load the model and set model parameters
             _prompt_ = "Loading model ... It might take some time to download the model if using for the 1st time.."
             conf_dict = load_yaml_config(_path)
             conf_dict["score_thres"] = score_threshold
@@ -126,10 +129,11 @@ def main() -> None:
                 # Get instance predictions for the uploaded Image
                 bb, lb, sc = get_preds(model, image)
 
-            st.markdown("## Results")
+            # Print Results
+            st.markdown("**Results:**")
             st.markdown(
-                f"> - **Number of bounding boxes detected in Image** : {len(bb)}\n"
-                f"- **Breeds detected in Image** : {[label_dict[idx] for idx in lb]}"
+                f"* **Number of bounding boxes detected in Image** : {len(bb)}\n"
+                f"* **Breeds detected in Image** : {[label_dict[idx] for idx in lb]}"
             )
 
             with st.spinner("Writing results on the Image:"):
